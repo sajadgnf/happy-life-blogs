8bde8f2658a537a6b09f86f8e7836b20d8f065fb
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-07-24 17:07:16 +0430

    get each blog info

--- src/components/blog/BlogPage.tsx
+++ src/components/blog/BlogPage.tsx
@@ -1,6 +1,17 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { GET_BLOG_INFO } from '../../graphql/queries';
 
 const BlogPage = () => {
+
+    const { slug } = useParams()
+
+    const { loading, data, error } = useQuery(GET_BLOG_INFO, { variables: { slug } })
+
+    console.log(data);
+    
+
     return (
         <div>
             bbb
